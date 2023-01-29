5fc704aa7761d5b27f27040ff744f6c40e4cfea9
Yoonseo Han <yooncer00@gmail.com>
2023-01-29 23:32:24 +0900

    Refactoring

--- App.js
+++ App.js
@@ -17,34 +17,19 @@ const options = {
 	}
 };
 
-// function getTeamInfo() {
-//     //Work on fetching data from API
-//     //console.log(config.url + 'teams?id=2');
-//     return fetch(config.url + 'teams?id=2', options) 
-//         .then(response => response.json())
-//         .then((jsonObject) => {
-//             //console.log(jsonObject['response'][0].logo);
-//             //return (jsonObject['response'][0].logo);
-            
-//             //SEnd message to discord
-//             return jsonObject;
-//         })
-//         .catch(err => console.log(err));
-// }
+const getTeamLogo = fetch(config.url + 'teams?id=2', options)
+    .then((response) => response.json())
+    .then(jsonObject => {
+        //console.log(jsonObject['response'][0].logo);
+        return jsonObject['response'][0].logo;
+    })
+    .catch(err => console.log(err));
 
 client.on("ready", () => {
     console.log("Discord bot Ready");
 });
 
 client.on("messageCreate", (message) => {
-    const getTeamLogo = fetch(config.url + 'teams?id=2', options)
-        .then((response) => response.json())
-        .then(jsonObject => {
-            //console.log(jsonObject['response'][0].logo);
-            return jsonObject['response'][0].logo;
-        })
-        .catch(err => console.log(err));
-    
     const printLogo = async () => {
         const teamLogo = await getTeamLogo;
         //message.channel.send(teamLogo);
