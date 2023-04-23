2c0121018f893a96469283efa3d94d24032d7f24
Yoonseo Han <yooncer00@gmail.com>
2023-04-23 17:48:58 +0800

    Add function to retrieve game score

---
+++ commands/Score.js
@@ -0,0 +1,12 @@
+//Command to retrieve score of game
+const { SlashCommandBuilder } = require('discord.js');
+const fetch = require('node-fetch-commonjs');
+const config = require('../config.json');
+
+const Sequelize = require('sequelize');
+
+
+//Export run method
+module.exports = {
+    
+};
